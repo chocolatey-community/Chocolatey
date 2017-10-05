5cbc2cb83fd909c2026b40012df1f7fcdacb61b0
gaelcolas <gaelcolas@users.noreply.github.com>
2017-10-05 22:40:20 +0100

    handling proxy in remote file and remote string

--- Chocolatey/private/Get-RemoteFile.ps1
+++ Chocolatey/private/Get-RemoteFile.ps1
@@ -2,10 +2,27 @@ function Get-RemoteFile {
     [CmdletBinding()]
     param (
         [string]$url,
-        [string]$file
+
+        [string]$file,
+
+        [uri]
+        $ProxyLocation,
+
+        [pscredential]
+        $ProxyCredential,
+
+        # To bypass the use of any proxy, please set IgnoreProxy
+        [switch]
+        $IgnoreProxy
     )
     
     Write-Debug "Downloading $url to $file"
-    $downloader = Get-Downloader $url
+    $downloaderParams = @{}
+    $KeysForDownloader = $PSBoundParameters.keys | Where-Object { $_ -notin @('file')}
+    foreach ($key in $KeysForDownloader ) { 
+        Write-Debug "`tWith $key :: $($PSBoundParameters[$key])"
+        $null = $downloaderParams.Add($key ,$PSBoundParameters[$key]) 
+    }
+    $downloader = Get-Downloader @downloaderParams
     $downloader.DownloadFile($url, $file)
 }
--- Chocolatey/private/Get-RemoteString.ps1
+++ Chocolatey/private/Get-RemoteString.ps1
@@ -1,9 +1,26 @@
 function Get-RemoteString {
     [CmdletBinding()]
     param (
-        [uri]$url
+        [string]$url,
+
+        [uri]
+        $ProxyLocation,
+
+        [pscredential]
+        $ProxyCredential,
+
+        # To bypass the use of any proxy, please set IgnoreProxy
+        [switch]
+        $IgnoreProxy
     )
-    Write-Debug "Downloading String from $url"
-    $downloader = Get-Downloader $url
+    
+    Write-Debug "Downloading string from $url"
+    $downloaderParams = @{}
+    $KeysForDownloader = $PSBoundParameters.keys | Where-Object { $_ -notin @()}
+    foreach ($key in $KeysForDownloader ) { 
+        Write-Debug "`tWith $key :: $($PSBoundParameters[$key])"
+        $null = $downloaderParams.Add($key,$PSBoundParameters[$key]) 
+    }
+    $downloader = Get-Downloader @downloaderParams
     return $downloader.DownloadString($url)
 }
