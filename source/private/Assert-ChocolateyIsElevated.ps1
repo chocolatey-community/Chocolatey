2354ef8b44efe255e89e4d9614185ae712e14101
gaelcolas <gaelcolas@synedgy.com>
2023-03-30 18:57:37 +0200

    fixing comment based help for assert-ChocolateyIsElevated

--- source/private/Assert-ChocolateyIsElevated.ps1
+++ source/private/Assert-ChocolateyIsElevated.ps1
@@ -1,3 +1,21 @@
+<#
+.SYNOPSIS
+    Make sure the current process is running elevated for Chocolatey.
+
+.DESCRIPTION
+    This command throws if the process is not running elevated.
+    This is useful when running in a default value of a switch parameter
+    to validate either that the running process is elevated, or that
+    the user wants to run non elevated anyway by toggling the switch.
+
+.EXAMPLE
+    [Parameter(DontShow)]
+    [switch]
+    $RunNonElevated = $(Assert-ChocolateyIsElevated)
+
+.NOTES
+    This is only for commands that change the system and needs Elevated permissions.
+#>
 function Assert-ChocolateyIsElevated
 {
     [CmdletBinding()]
